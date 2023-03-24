ca8a33c4ec4cbd3464c808f9f986bdc0a51c62c3
Colton <allen.colton@hotmail.com>
2023-03-23 20:13:45 -0700

    changed to static adapter

---
+++ build/_app/immutable/chunks/1.c5f2283e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.6c01a8d8.js";export{t as component};
---
+++ build/_app/immutable/chunks/2.299afcf0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.36fe5957.js";export{t as component};
